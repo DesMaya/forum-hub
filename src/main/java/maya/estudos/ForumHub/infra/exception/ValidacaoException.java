c87b229efc36f6d826e6fad0eacef045fe716e91
Maya <mayaflorentinodasilva@gmail.com>
2025-01-17 19:34:39 -0300

    Criada a exception personalizada para validação

---
+++ src/main/java/maya/estudos/ForumHub/infra/exception/ValidacaoException.java
@@ -0,0 +1,8 @@
+package maya.estudos.ForumHub.infra.exception;
+
+public class ValidacaoException extends RuntimeException{
+
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
