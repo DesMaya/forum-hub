6c251f989308a0a02e7b1398a01e594021948c4e
Maya <mayaflorentinodasilva@gmail.com>
2025-01-17 19:33:56 -0300

    Inserindo um usuário inicial no banco de dados

---
+++ src/main/resources/db/migration/V2__populate_usuarios_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuarios (ativo, email, nome, senha)
+VALUES (
+    true, -- usuário ativo
+    'maya.florentino@gmail.com',
+    'Maya Florentino',
+    '$2a$12$7zxzCxTWx6tMPjfi/InpGeKoaN0IjF6X2R8C4w9XoB6KiTQ1UBCsi'
+
+);
