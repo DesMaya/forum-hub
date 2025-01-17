5fe6741b30a4e17fdb7d7b931a71d215c3a458ec
Maya <mayaflorentinodasilva@gmail.com>
2025-01-17 18:06:44 -0300

    Implementada a classe Topico e sua estrutura

--- src/main/java/maya/estudos/ForumHub/domain/topico/Topico.java
+++ src/main/java/maya/estudos/ForumHub/domain/topico/Topico.java
@@ -1,6 +1,135 @@
 package maya.estudos.ForumHub.domain.topico;
 
-, conf
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@EqualsAndHashCode(of = "id")
 public class Topico {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusTopico status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Usuario autor;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+
+    @OneToMany(mappedBy = "topico", fetch = FetchType.LAZY)
+    private List<Resposta> respostas = new ArrayList<>();
+
+
+    public List<DadosRespostaResponse> getRespostas() {
+        return respostas.stream().map(DadosRespostaResponse::new).toList();
+    }
+
+    public void atualizar(DadosTopicoAtualizacao atualizacao) {
+
+        if (atualizacao.titulo() != null) {
+            this.titulo = atualizacao.titulo();
+        }
+
+        if (atualizacao.mensagem() != null) {
+            this.mensagem = atualizacao.mensagem();
+        }
+    }
+
+    public void setStatus(StatusTopico statusTopico) {
+        this.status = statusTopico;
+    }
+
+
+
+    public Topico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao, StatusTopico status, Usuario autor, Curso curso, List<Resposta> respostas) {
+        this.id = id;
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+        this.dataCriacao = dataCriacao;
+        this.status = status;
+        this.autor = autor;
+        this.curso = curso;
+        this.respostas = respostas;
+    }
+
+    public Topico() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public StatusTopico getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTopico status) {
+        this.status = status;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public List<Resposta> getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(List<Resposta> respostas) {
+        this.respostas = respostas;
+    }
 }
