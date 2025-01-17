63e65e1b8862d10ddc0684694ba1bdcb7635bb45
Maya <mayaflorentinodasilva@gmail.com>
2025-01-17 18:06:59 -0300

    Criados os enums a serem utilizados por topico

---
+++ src/main/java/maya/estudos/ForumHub/domain/topico/Categoria.java
@@ -0,0 +1,13 @@
+package maya.estudos.ForumHub.domain.topico;
+
+public enum Categoria {
+    PROGRAMACAO,
+    REDES,
+    BANCO_DE_DADOS,
+    INFRAESTRUTURA,
+    DESIGN,
+    AGILE,
+    HARDWARE,
+    SOFTWARE,
+    OUTROS
+}
