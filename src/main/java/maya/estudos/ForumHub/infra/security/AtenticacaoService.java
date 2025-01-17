07a042d4f7cea5d98fd98dcc0e641667ce33ccf8
Maya <mayaflorentinodasilva@gmail.com>
2025-01-17 19:35:17 -0300

    Criada as classes de autenticação

---
+++ src/main/java/maya/estudos/ForumHub/infra/security/AtenticacaoService.java
@@ -0,0 +1,20 @@
+package maya.estudos.ForumHub.infra.security;
+
+import maya.estudos.ForumHub.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AtenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username);
+    }
+}
